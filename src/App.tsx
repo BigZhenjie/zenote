caaefd4f09d98adca828b44a4404e08847db75e5
BigZhenjie <1741zhenjie@gmail.com>
2024-11-26 20:59:32 -0800

    adds a loading circle component for loading animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import "./App.css";
-import Login from "./pages/Login";
+import { lazy, Suspense } from "react";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import LoadingCircle from "./components/LoadingCircle";
+
+// Lazy imports
+const Login = lazy(() => import("./pages/Login"));
 function App() {
-  return <Login />;
+  return (
+    <HashRouter>
+      <Suspense
+        fallback={
+          <div className="h-screen w-screen">
+            <LoadingCircle />
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </Suspense>
+    </HashRouter>
+  );
 }
 
 export default App;
