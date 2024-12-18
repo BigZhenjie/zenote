af11f7d9b085591f32791547a7979864df03bd0c
BigZhenjie <1741zhenjie@gmail.com>
2024-12-17 21:04:42 -0800

    feat: adds dotenv and init supabase

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+mod supabase;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
---
+++ src-tauri/src/supabase.rs
@@ -0,0 +1,13 @@
+use supabase_rs::SupabaseClient;
+
+use dotenv::dotenv;
+use std::env::var;
+
+async fn initialize_supabase_client() -> SupabaseClient {
+    dotenv().ok(); // Load the .env file
+
+    let supabase_client: SupabaseClient =
+        SupabaseClient::new(var("SUPABASE_URL").unwrap(), var("SUPABASE_KEY").unwrap()).unwrap();
+
+    supabase_client
+}
