d1c071931134131a519354e14a49b511bc7d04e0
BigZhenjie <1741zhenjie@gmail.com>
2025-02-06 20:36:30 -0800

    style: adds more padding to each primary sidebar item

--- src/components/home/sidebar/SidebarItem.tsx
+++ src/components/home/sidebar/SidebarItem.tsx
@@ -3,7 +3,7 @@ import { SidebarItemProps } from "@/types";
 const SidebarItem = ({ title, Icon, path }: SidebarItemProps) => {
   return (
     <div>
-      <button className="w-full py-1 hover:bg-gray-100 rounded-md text-gray-600 flex items-center gap-3">
+      <button className="w-full py-1 px-2 hover:bg-gray-100 rounded-md text-gray-600 flex items-center gap-3">
         <Icon size={20} />
         <p className="font-medium text-[14px]">{title}</p>
       </button>
