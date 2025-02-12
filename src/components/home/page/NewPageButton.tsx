cd27c3c9d66a8214d8fa5b8e667706ebd1526530
BigZhenjie <1741zhenjie@gmail.com>
2025-02-11 20:03:36 -0800

    style: removes z index of 10 from new page button

--- src/components/home/page/NewPageButton.tsx
+++ src/components/home/page/NewPageButton.tsx
@@ -4,7 +4,7 @@ const NewPageButton = () => {
   return (
     <button className="relative w-36 h-36 rounded-2xl border-[1px] hover:border-gray-300 hover:shadow-md">
         <div className="absolute top-0 left-0 right-0 h-[30%] bg-ze-light rounded-t-2xl"/>
-        <div className="absolute top-[30px] left-7 border-[3px] rounded-lg w-6 h-6 flex items-center justify-center z-10">
+        <div className="absolute top-[30px] left-7 border-[3px] rounded-lg w-6 h-6 flex items-center justify-center">
             <Plus className="text-[#37352f] opacity-25" size={20}/>
         </div>
         <p className="text-gray-400 mt-4 w-4/5 text-sm font-medium">
