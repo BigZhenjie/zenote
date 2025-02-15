8fead97337c67a2b480ec4d09a19d466b0e90d65
BigZhenjie <1741zhenjie@gmail.com>
2025-02-14 21:55:22 -0800

    feat: add navigation functionality to SidebarItem component

--- src/components/home/sidebar/SidebarItem.tsx
+++ src/components/home/sidebar/SidebarItem.tsx
@@ -1,9 +1,14 @@
 import { SidebarItemProps } from "@/types";
-
+import { useNavigate } from "react-router-dom";
 const SidebarItem = ({ title, Icon, path, isActive }: SidebarItemProps) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(path);
+  };
   return (
     <div>
-      <button className={`w-full py-1 px-2 hover:bg-gray-100 rounded-md text-gray-600 flex items-center gap-3 ${isActive ? 'bg-gray-100' : ''}`}>
+      <button className={`w-full py-1 px-2 hover:bg-gray-100 rounded-md text-gray-600 flex items-center gap-3 ${isActive ? 'bg-gray-100' : ''}` } onClick={handleClick}>
         <Icon size={20} />
         <p className="font-medium text-[14px]">{title}</p>
       </button>
