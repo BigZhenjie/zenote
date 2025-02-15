c6c870529ed9998cb6be24d35e585c7dbf950e96
BigZhenjie <1741zhenjie@gmail.com>
2025-02-15 09:17:43 -0800

    feat: integrate Layout component into routing structure and enhance Page styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import "./App.css";
 import { lazy, Suspense } from "react";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import LoadingCircle from "./components/LoadingCircle";
+import Layout from "./components/Layout";
 import Home from "./pages/Home";
+import { Outlet } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
-import Layout from "./components/Layout"; // Importing Layout component
 import Page from "./pages/Page";
 // Lazy imports
 const Login = lazy(() => import("./pages/Login"));
 const OnBoarding = lazy(() => import("./pages/OnBoarding"));
+
 function App() {
   return (
     <AuthProvider>
@@ -20,16 +22,16 @@ function App() {
             </div>
           }
         >
-          <Layout>
-            <Routes>
+          <Routes>
             <Route path="/" element={<Login />} />
-            <Route path="/onboarding/:email" element={<OnBoarding />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/:pageId" element={<Page />} />
-            {/*route for 404*/}
-            <Route path="*" element={<div>404</div>} />
-            </Routes>
-          </Layout>
+            <Route element={<Layout children={<Outlet />} />}>
+              <Route path="/onboarding/:email" element={<OnBoarding />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/:pageId" element={<Page />} />
+              {/*route for 404*/}
+              <Route path="*" element={<div>404</div>} />
+            </Route>
+          </Routes>
         </Suspense>
       </HashRouter>
     </AuthProvider>
--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -5,7 +5,7 @@ const Page = () => {
   const { pageId } = useParams<{ pageId: string }>();
 
   return (
-    <div>
+    <div className='w-full p-8 overflow-y-auto flex flex-col items-center rounded-xl ml-4 bg-white'>
       <h1>Page ID: {pageId}</h1>
       <p>This is the content for the page with ID: {pageId}</p>
     </div>
