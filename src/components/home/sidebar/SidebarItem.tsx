a5a898e55c45ed3de3f47bf514449264f2d76d97
BigZhenjie <1741zhenjie@gmail.com>
2025-02-15 09:55:41 -0800

    style: adds active sidebar item color

--- src/components/home/sidebar/SidebarItem.tsx
+++ src/components/home/sidebar/SidebarItem.tsx
@@ -10,7 +10,7 @@ const SidebarItem = ({ title, Icon, path, isActive }: SidebarItemProps) => {
   };
   return (
     <div>
-      <button className={`w-full py-1 px-2 hover:bg-gray-100 rounded-md text-gray-600 flex items-center gap-3 ${isActive ? 'bg-gray-100' : ''}` } onClick={handleClick}>
+      <button className={`w-full py-1 px-2 hover:bg-gray-50 active:bg-gray-300 rounded-md text-gray-600 flex items-center gap-3 ${isActive ? 'bg-gray-50' : ''}` } onClick={handleClick}>
         <Icon size={20} />
         <p className="font-medium text-[14px]">{title}</p>
       </button>
