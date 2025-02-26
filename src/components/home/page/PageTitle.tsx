df97e01d4b477c05aa149c54e632abc6ca424493
BigZhenjie <1741zhenjie@gmail.com>
2025-02-25 21:02:38 -0800

    style: adds a height to page title to prevent cut off of bottom

--- src/components/home/page/PageTitle.tsx
+++ src/components/home/page/PageTitle.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const PageTitle = ({title, setTitle}: {title: string, setTitle: React.Dispatch<React.SetStateAction<string>>}) => {
   return (
-    <div className="max-w-[80%] w-[80%]">
+    <div className="max-w-[80%] w-[80%] ">
     <input
       value={title}
       onChange={(e) => setTitle(e.target.value)}
       placeholder="New page"
       spellCheck="false"
-      className="mt-20 text-4xl p-2 outline-none font-bold placeholder:font-bold placeholder:opacity-40"
+      className="mt-20 text-4xl p-2 h-14 outline-none font-bold placeholder:font-bold placeholder:opacity-40"
     ></input>
   </div>
   )
