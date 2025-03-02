5730c52afb94b9ed643756318fd9ba56aaf47b7a
BigZhenjie <1741zhenjie@gmail.com>
2025-03-02 11:02:49 -0800

    style: hides long title in page squares

--- src/components/home/page/PageSquare.tsx
+++ src/components/home/page/PageSquare.tsx
@@ -37,14 +37,14 @@ const PageSquare = ({
 
   return (
     <button 
-      className="relative min-w-36 min-h-36 rounded-2xl border-[1px] hover:border-gray-300 hover:shadow-sm"
+      className="relative w-36 min-h-36 rounded-2xl border-[1px] hover:border-gray-300 hover:shadow-sm"
       onClick={handleClick}
     >
       <div className="absolute top-0 left-0 right-0 h-[30%] bg-ze-light rounded-t-2xl"/>
       <div className="absolute top-[30px] left-7 border-[3px] rounded-lg w-6 h-6 flex items-center justify-center">
         <FileText className="text-[#37352f] opacity-25" size={20}/>
       </div>
-      <p className="text-gray-400 mt-4 w-4/5 text-sm font-medium">
+      <p className="text-gray-400 mt-4 w-4/5 text-sm truncate text-center">
         {title}
       </p>
       <div className="absolute bottom-2 left-2 flex items-center gap-2">
