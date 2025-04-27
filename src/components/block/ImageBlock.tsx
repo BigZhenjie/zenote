c43f5003d7c627960f14419b9e460ccb1c7a4c94
BigZhenjie <1741zhenjie@gmail.com>
2025-04-27 16:36:42 -0700

    feat: added some block functionality to image blocks

--- src/components/block/ImageBlock.tsx
+++ src/components/block/ImageBlock.tsx
@@ -1,5 +1,8 @@
-import React from 'react'
-
+import { useState, useEffect, useCallback } from "react";
+import { Plus, Trash2 } from "lucide-react";
+import { debounce} from "lodash";
+import { BlockProps, Response } from "@/types";
+import { invoke } from "@tauri-apps/api/core";
 type OptionalBlockProps = {
   id?: string;
   created_at?: string;
@@ -19,17 +22,46 @@ const ImageBlock = ({
   id,
   pageId,
   parentBlockId,
-  content,
+  content = "",
   type = "text",
   order,
   blocks,
   setBlocks,
 }: OptionalBlockProps) => {
-  return (
-    <div>
-      <img src={content} alt="" />
-    </div>
-  )
-}
+  const [isHovered, setIsHovered] = useState(false);
+  const deleteBlock = async (id: string) => {
+    try {
+      await invoke("delete_block", { blockId: id });
+      setBlocks(prevBlocks => prevBlocks.filter(block => block.id !== id));
+    } catch (error) {
+      console.error("Failed to delete block:", error);
+    }
+  }
 
-export default ImageBlock
+  return (
+    <div
+      className="w-[80%] flex items-center relative mb-2"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      {isHovered && (
+        <div className="absolute left-0 translate-x-[-100%] flex justify-end">
+        <Plus
+          size={25}
+          className="hover:bg-gray-100 rounded-md p-1"
+        />
+        {id && <Trash2 
+          size={25}
+          className=" hover:bg-gray-100 rounded-md p-1"
+          onClick={() => deleteBlock(id)}
+        />}
+      </div>
+      )}
+
+      <img src={content} alt="" />
+      
+    </div>
+  );
+};
+
+export default ImageBlock;
